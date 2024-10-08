8d106ec313d0b07725240f0e78fb410c4811ef22
Neo <neogoldahlgren@gmail.com>
2024-10-08 11:47:29 +0200

    första funktionen
    Skapade den första funktionen som ska köras om användaren bettar på "Röd"

--- script.js
+++ script.js
@@ -15,3 +15,35 @@ redB.addEventListener("click", redBet)
 blackB.addEventListener("click", blackBet)
 greenB.addEventListener("click", greenBet)
 reset.addEventListener("click", resetBalance)
+
+function redBet() {
+    let userBet = red.value 
+    var num = Math.floor(Math.random() * 37)
+
+    if (userBet <= 0) {
+        infoPara.innerHTML = `Your bet must be higher than 0! Try again.`
+    } else if (userBet > balance) {
+        infoPara.innerHTML = `Your bet is more than your balance! Try again!`
+    } else {
+        if (num === 0 || num === 36) {
+            infoPara.innerHTML = `You lost! The colour was green.<br>The number was ${num}`
+            balance = balance - userBet
+            balanceHTML.innerHTML = `Balance: ${balance}`
+            red.value = ""
+        } else if (num > 17) {
+            infoPara.innerHTML = `You lost! The colour was black.<br>The number was ${num}`
+            balance = balance - userBet
+            balanceHTML.innerHTML = `Balance: ${balance}`
+            red.value = ""
+        } else {
+            balance = balance - userBet
+            infoPara.innerHTML = `You Won! The colour was red.<br>The number was ${num}`
+            userBet = userBet * 2
+            balance = balance + userBet
+            balanceHTML.innerHTML = `Balance: ${balance}`
+            red.value = ""
+        }
+    }
+
+    
+}
