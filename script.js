e2ae09e49abc7df0184b88a82a5d291710545672
Neo <neogoldahlgren@gmail.com>
2024-10-08 11:48:52 +0200

    Skrev till de andra funktionerna
    Skrev till de två andra funktionerna för bettingen. (Om användaren bettar Svart eller Grön)
    
    avslutade med en funktion som låter användaren återställa sin balance

--- script.js
+++ script.js
@@ -47,3 +47,69 @@ function redBet() {
 
     
 }
+
+function blackBet() {
+    let userBet = black.value 
+    var num = Math.floor(Math.random() * 37)
+
+    if (userBet <= 0) {
+        infoPara.innerHTML = `Your bet must be higher than 0! Try again.`
+    } else if (userBet > balance) {
+        infoPara.innerHTML = `Your bet is more than your balance! Try again!`
+    } else {
+        if (num === 0 || num === 36) {
+            infoPara.innerHTML = `You lost! The colour was green.<br>The number was ${num}`
+            balance = balance - userBet
+            balanceHTML.innerHTML = `Balance: ${balance}`
+            black.value = ""
+        } else if (num > 17) {
+            balance = balance - userBet
+            infoPara.innerHTML = `You Won! The colour was black.<br>The number was ${num}`
+            userBet = userBet * 2
+            balance = balance + userBet
+            balanceHTML.innerHTML = `Balance: ${balance}`
+            black.value = ""
+        } else {
+            infoPara.innerHTML = `You lost! The colour was red.<br>The number was ${num}`
+            balance = balance - userBet
+            balanceHTML.innerHTML = `Balance: ${balance}`
+            black.value = ""
+        }
+    }
+}
+
+function greenBet() {
+    let userBet = green.value
+    var num = Math.floor(Math.random() * 37)
+
+    if (userBet <= 0) {
+        infoPara.innerHTML = `Your bet must be higher than 0! Try again.`
+    } else if (userBet > balance) {
+        infoPara.innerHTML = `Your bet is more than your balance! Try again!`
+    } else {
+        if (num === 0 || num === 36) {
+            balance = balance - userBet
+            infoPara.innerHTML = `You Won! The colour was GREEN!.<br>The number was ${num}`
+            userBet = userBet * 14
+            balance = balance + userBet
+            balanceHTML.innerHTML = `Balance: ${balance}`
+            green.value = ""
+        } else if (num > 17) {
+            infoPara.innerHTML = `You lost! The colour was black.<br>The number was ${num}`
+            balance = balance - userBet
+            balanceHTML.innerHTML = `Balance: ${balance}`
+            green.value = ""
+        } else {
+            infoPara.innerHTML = `You lost! The colour was red.<br>The number was ${num}`
+            balance = balance - userBet
+            balanceHTML.innerHTML = `Balance: ${balance}`
+            green.value = ""
+        }
+    }
+}
+
+function resetBalance() {
+    balance = 1000
+    balanceHTML.innerHTML = `Balance: ${balance}`
+    infoPara.innerHTML = `Your balance has been reset!`
+}
